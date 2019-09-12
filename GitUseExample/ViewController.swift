020f2ff5c258fb22d1a47da323cf49893b177a97
Austin Herman <ashe223@g.uky.edu>
2019-09-12 12:52:26 -0400

    initial commit

---
+++ GitUseExample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GitUseExample
+//
+//  Created by  on 9/12/19.
+//  Copyright © 2019 Austin Herman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttontapped(_ sender: Any) {
+        print("example button tapped")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("this is a git tutorial")
+    }
+
+
+}
+
